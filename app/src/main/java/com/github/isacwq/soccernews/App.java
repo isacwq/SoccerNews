6626c89c4546105cb7188a5fab174a62ca5b9496
Isac Tomaz da Silva <isactomaz@protonmail.com>
2022-08-15 22:58:04 -0300

    adding pt br values, repo and viewmodel refactor

---
+++ app/src/main/java/com/github/isacwq/soccernews/App.java
@@ -0,0 +1,17 @@
+package com.github.isacwq.soccernews;
+
+import android.app.Application;
+
+public class App extends Application {
+    private static App instance;
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+}
--- app/src/main/java/com/github/isacwq/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/com/github/isacwq/soccernews/ui/news/NewsFragment.java
@@ -11,33 +11,57 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.github.isacwq.soccernews.MainActivity;
+import com.github.isacwq.soccernews.R;
 import com.github.isacwq.soccernews.databinding.FragmentNewsBinding;
 import com.github.isacwq.soccernews.ui.adapter.NewsAdapter;
+import com.google.android.material.snackbar.Snackbar;
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
+    private NewsViewModel newsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel =
-                new ViewModelProvider(this).get(NewsViewModel.class);
+        newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         binding.recyclerNews.setLayoutManager(new LinearLayoutManager(getContext()));
+        observeNews();
+        setupStates();
+        binding.swipeRefreshNews.setOnRefreshListener(newsViewModel::findNews);
+        return root;
+    }
+
+    private void observeNews() {
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
             binding.recyclerNews.setAdapter(new NewsAdapter(news, favoritesNews -> {
-                Log.d("button", "like");
-                MainActivity activity = (MainActivity) getActivity();
-                if (activity != null) {
-                    activity.getDb().newsDao().insert(favoritesNews);
-                }
+                Log.d("SOCCER_NEWS", "button like");
+                newsViewModel.insertNews(favoritesNews);
             }));
         });
-        return root;
+    }
+
+    private void setupStates() {
+        newsViewModel.getState().observe(getViewLifecycleOwner(), state -> {
+            switch (state) {
+                case LOADING:
+                    binding.swipeRefreshNews.setRefreshing(true);
+                    break;
+                case SUCCESS:
+                    binding.swipeRefreshNews.setRefreshing(false);
+                    break;
+                case ERROR:
+                    binding.swipeRefreshNews.setRefreshing(false);
+                    Snackbar.make(
+                            binding.swipeRefreshNews,
+                            R.string.error_network,
+                            Snackbar.LENGTH_SHORT
+                    ).show();
+            }
+        });
     }
 
     @Override
